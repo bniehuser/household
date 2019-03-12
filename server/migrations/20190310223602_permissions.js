d982e54408f1fcbd6705f0b7e8c3498d644e3e9c
Barry Niehuser <bniehuser@gmail.com>
2019-03-11 18:29:03 -0700

    finished and migrated permissions

--- server/migrations/20190310223602_permissions.js
+++ server/migrations/20190310223602_permissions.js
@@ -2,25 +2,47 @@ const {onUpdateTrigger,dropUpdateTrigger} = require("../knexfile");
 
 exports.up = async (knex) => {
 
-    await knex.schema.createTable('Role', (table) => {
-        table.increments('id').primary();
-        table.string('email');
-        table.string('firstName');
-        table.string('lastName');
-        table.string('password');
-        table.timestamps(true, true);
-    }).then(() => knex.raw(onUpdateTrigger('Role')));
-
-    await knex.schema.createTable('UserRole', (table) => {
-        table.increments('id').primary();
-        table.string('email');
-        table.string('firstName');
-        table.string('lastName');
-        table.string('password');
-        table.timestamp('createdAt').defaultTo(knex.fn.now(6));
-    });
+    await knex.schema
+        .createTable('Feature', (table) => {
+            table.increments('id').primary();
+            table.integer('parentId').references('id').inTable('Feature').onDelete('cascade');
+            table.string('name').notNullable();
+            table.text('description');
+            table.timestamps(true, true);
+        })
+        .createTable('Role', (table) => {
+            table.increments('id').primary();
+            table.string('name').notNullable();
+            table.text('description');
+            table.timestamps(true, true);
+        })
+        .createTable('UserRole', (table) => {
+            table.increments('id').primary();
+            table.integer('userId').references('id').inTable('User').notNullable().onDelete('cascade');
+            table.integer('roleId').references('id').inTable('Role').notNullable().onDelete('cascade');
+            table.timestamps(true, true);
+        })
+        .createTable('RoleFeaturePermission', (table) => {
+            table.increments('id').primary();
+            table.integer('roleId').references('id').inTable('Role').notNullable().onDelete('cascade');
+            table.integer('featureId').references('id').inTable('Feature').notNullable().onDelete('cascade');
+            table.string('permission').notNullable();
+            table.timestamps(true, true);
+        })
+        .then(() => {
+            knex.raw(onUpdateTrigger('Feature'));
+            knex.raw(onUpdateTrigger('Role'));
+            knex.raw(onUpdateTrigger('UserRole'));
+            knex.raw(onUpdateTrigger('RoleFeaturePermission'));
+        })
+    ;
 };
 
 exports.down = async (knex) => {
-    await knex.schema.dropTable('User');
+    await knex.schema
+        .dropTable('RoleFeaturePermission')
+        .dropTable('UserRole')
+        .dropTable('Role')
+        .dropTable('Feature')
+    ;
 };
